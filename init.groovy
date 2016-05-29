3f43ccb6674d30d5ea4013a38a391f3d7c6e253c
crosson.david@gmail.com <crosson.david@gmail.com>
2016-05-29 11:46:11 +0200

    dsl example enhancements

--- init.groovy
+++ init.groovy
@@ -1,11 +1,14 @@
-
-
 job("mtest-dep") {
   scm {
-    git("http://10.236.246.220:9090/crodav/mtest-dep.git")
+    git {
+      remote {
+        url("http://10.236.246.220:9090/crodav/mtest-dep.git")
+      }
+      branch('master')
+    }
   }
   triggers {
-    scm('*/2 * * * *')
+    scm('H/2 * * * *')
   }
   steps {
       maven('clean test install')
@@ -16,12 +19,25 @@ job("mtest-dep") {
 
 job("mtest-web-project") {
   scm {
-    git("http://10.236.246.220:9090/crodav/mtest-web-project.git")
+    git {
+      remote {
+        url("http://10.236.246.220:9090/crodav/mtest-web-project.git")
+      }
+      branch('master')
+    }
   }
   triggers {
-      scm('*/2 * * * *')
+      scm('H/2 * * * *')
   }
   steps {
       maven('clean package')
+  }
+}
+
+
+listView('mtest') {
+  jobs {
+    name('mtest-dep')
+    name('mtest-web-project')
   }
 }

206d8cf01cbfb3b66acfe534588689680d4aa2e4
crosson.david@gmail.com <crosson.david@gmail.com>
2016-05-29 11:21:37 +0200

    first commit

---
+++ init.groovy
@@ -0,0 +1,33 @@
+job("mtest-dep") {
+  scm {
+      git {
+          remote {
+            github("http://10.236.246.220:9090/crodav/mtest-dep.git")
+          }
+          createTag(false)
+      }
+  }
+  triggers {
+      scm('*/2 * * * *')
+  }
+  steps {
+      maven('-e clean test install')
+  }
+}
+
+job("mtest-web-project") {
+  scm {
+      git {
+          remote {
+            github("http://10.236.246.220:9090/crodav/mtest-web-project.git")
+          }
+          createTag(false)
+      }
+  }
+  triggers {
+      scm('*/2 * * * *')
+  }
+  steps {
+      maven('-e clean package')
+  }
+}
